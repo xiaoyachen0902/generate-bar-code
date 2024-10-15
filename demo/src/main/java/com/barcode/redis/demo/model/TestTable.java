f59de374826274d12f8b7135eebaaa0ec0a4122e
Erminia (Jitera) <developers@jitera.com>
2024-10-15 14:28:18 +0000

    feat[database]: create table 'testtables' Changes * '/demo/src/main/java/com/barcode/redis/demo/model/TestTable.java': created * '/demo/src/main/resources/db/migration/V1729002483678__Create_testtables_table.sql': created

---
+++ demo/src/main/java/com/barcode/redis/demo/model/TestTable.java
@@ -0,0 +1,48 @@
+package com.barcode.redis.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class TestTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
